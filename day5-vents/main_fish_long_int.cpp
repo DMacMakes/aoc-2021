6593ad646e9963f37281074433b1bcfb3208c70f
dmacmakes <dmacmakes@gmail.com>
2022-01-17 05:09:28 +0000

    8 threads using futures and async. Still died attempting 200 days

---
+++ day5-vents/main_fish_long_int.cpp
@@ -0,0 +1,378 @@
+/********************************************************************
+* Vents - Advent of Code day 5.
+*********************************************************************
+* 
+* Vents are linear on ocean floor, mapped as a grid. An input file 
+* describes them as beginning and end points, e.g., 0,9 -> 5,9
+* Meaning: Two points, x,y cords.
+* For Part A, find the horizontal or vertical vents, map those on 
+* a grid. Find which cells in the floor grid have more than one
+* vent present (overlap). See example below. Some can be colinear,
+* which at first makes no sense since they're the same vent, but
+* then the resolution of the grid could be quite low compared to 
+* vent widths. A 300x100 km stretch of ocean floor might be 3
+* cells across, and two vents could be parallel with 10s of kms 
+* separation:
+* X X 1 X X
+* 1 1 2 1 X
+* X X 1 X X
+* X X 2 2 2
+* 
+* New Approach:
+* Try a fast and loose approach this time; part B and diagonal
+* lines could be a thing.
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <array>
+#include <fstream>
+#include <set>
+#include <cmath>
+#include "include/ise102.h"
+#include "include/Point2d.h"
+#include "include/Cell.h"
+using namespace std;
+using scn::scan;
+using textpixels::Point2d;
+using std::ifstream;
+
+enum Styles
+{
+  LOUD_FRIENDLY,
+  STONE,
+  RED_ALERT
+};
+
+const string VENTS_FILE = "vent-line-locations.txt";
+std::map<int, fmt::text_style> text_styles {};
+
+// Create colour and emphasis styles that work with the fmt library
+// (print(), fmt() etc) and place them in the text_styles map.
+void createStyles()
+{
+  //-------------------------------------------------------- Create Speech Styles
+  // Add a text style to the text_styles collection.
+  // name it first in the Styles enum, and use that as the insert position.
+  // the makeStyle function uses the fmt library to build one.
+  text_styles.insert({
+    Styles::LOUD_FRIENDLY,
+    makeStyle(color::black, color::pale_golden_rod, emphasis::bold)
+    });
+  // I created a custom charcoal grey here with an rgb (red green blue) colour 
+  // in hex. You drop the six digits into `color(0x______)` as i've done below.
+  // Pick colours at https://www.w3schools.com/colors/colors_picker.asp
+  text_styles.insert({
+    Styles::STONE,
+    makeStyle(color(0x222222), color::silver)
+    });
+
+  text_styles.insert({
+    Styles::RED_ALERT,
+    makeStyle(color::white, color(0xDD0033), emphasis::bold)
+    });
+}
+
+// Demo/test a few text styles from the text_styles map, along with
+// clearScreen and delay (which can require buffer flush in delay)
+void greetInColour()
+{
+  clearScreen();
+  //----------------------------------------------------- Encounters
+  string town{ "Litsbane" };
+  print("\tSUNFLOWER: ");
+  print(text_styles[LOUD_FRIENDLY], " Welcome to {0}.!! \n\n", town);
+  delay(2000);
+  print("\tSTONE DOOR: ");
+  print(text_styles[STONE], strToUpper(" Hello mushy one, do not be surprised! \n"));
+  print("\n");
+  print(text_styles[RED_ALERT], "Bye Harvey.");
+  delay(1000);
+}
+
+// A vent has a beginning point and end point, each two dimensional (x,y)
+struct Vent
+{
+  Point2d loc_begin{};
+  Point2d loc_end{};
+  bool is_horiz = false;
+  bool is_vert = false;
+};
+
+// A rectangle defined by two points on an x,y plane, the top left
+// and bottom right extents.
+struct RectExtents
+{
+  Point2d top_left{};
+  Point2d bot_right{};
+};
+
+
+
+
+// If we drew a bounding box around all the vents on the ocean
+// floor, what would the location of the top left and bottom right
+// corners be? What are the min and max values for x and y?
+RectExtents getVentGridExtents(vector<Vent>& vents)
+{
+  // Start out with the x,y coordinates of the first vent, 
+  // work out from there.
+  static RectExtents extents{ vents[0].loc_begin, vents[0].loc_end };
+
+  for (auto& vent : vents)
+  {
+    // if either end of the vent is further up or left than the current top
+    // left extent, make that the new top or left extent.
+    if (extents.top_left.x > vent.loc_begin.x) extents.top_left.x = vent.loc_begin.x;
+    if (extents.top_left.x > vent.loc_end.x) extents.top_left.x = vent.loc_end.x;
+    if (extents.top_left.y > vent.loc_begin.y) extents.top_left.y = vent.loc_begin.y;
+    if (extents.top_left.y > vent.loc_end.y) extents.top_left.y = vent.loc_end.y;
+
+    // If either end of vent is further down or right than the current bottom
+    // right extent, make that the new bottom or right extent. (get lowest x or y)
+    if (extents.bot_right.x < vent.loc_begin.x) extents.bot_right.x = vent.loc_begin.x;
+    if (extents.bot_right.x < vent.loc_end.x) extents.bot_right.x = vent.loc_end.x;
+    if (extents.bot_right.y < vent.loc_begin.y) extents.bot_right.y = vent.loc_begin.y;
+    if (extents.bot_right.y < vent.loc_end.y) extents.bot_right.y = vent.loc_end.y;
+  }
+
+  return extents;
+}
+
+vector<Vent> getVentsFromFile(string file_path)
+{
+  // Grab the file handle in a way scn can keep it alive.
+  scn::owning_file vents_file(file_path.c_str(), "r");
+  vector<Vent> vents{};
+  //vents.reserve(1000);
+  string line{};
+  auto result = scn::make_result(vents_file);
+
+  do
+  {
+    result = scn::getline(result.range(), line, '\n');
+    string bin{};
+    Vent vent{};
+    //int a, b, c, d;
+    scn::scan(line, "{0},{1} -> {2},{3}", 
+                vent.loc_begin.x, vent.loc_begin.y, vent.loc_end.x, vent.loc_end.y);
+    // scan in the points on either side of the arrow, 
+    //print(text_styles[RED_ALERT], "Got a line:", line);
+    //print("start: x={0} y={1}", vent.loc_begin.x, vent.loc_begin.y);
+    //print("\tend: x={0} y={1} \n", vent.loc_end.x, vent.loc_end.y);
+    vents.push_back(vent);
+  } while (!result.empty()); // line != "");
+  //vents.shrink_to_fit();
+  // Clean up memory/file handles
+  //if(vents_file.is_open()) vents_file.close();
+
+  return vents;
+}
+
+// Checks if a vent has the same beginning and ending y values,
+// making it a vertical line in our grid
+bool isVertical(Vent& vent)
+{
+  return (vent.loc_begin.x == vent.loc_end.x);
+}
+
+// Checks if the vent has the same beginning and ending x values,
+// making it a horizontal line in our grid
+bool isHorizontal(Vent& vent)
+{
+  return (vent.loc_begin.y == vent.loc_end.y);
+}
+
+vector<Vent> getHorizontalAndVerticalVents(vector<Vent>& all_vents)
+{
+  // if begin.x == end.x || begin.y ==
+  vector<Vent> hv_vents{};
+  //hv_vents.reserve(all_vents.size());  // hold up to all vents
+  
+  for (Vent& vent : all_vents)
+  {
+      vent.is_vert = isVertical(vent);
+      vent.is_horiz = isHorizontal(vent);
+      if (vent.is_vert || vent.is_horiz)
+      {
+        hv_vents.push_back(vent);
+      }
+  }
+  //hv_vents.shrink_to_fit();
+  return hv_vents;
+}
+
+// A single vent is a line that crosses multiple cells in a grid drawn
+// on the floor of the ocean. The input data provides a beginning point
+// and an end. This function creates a collection of points covering
+// the entire length of a provided vent (currently only horizontal and
+// vertical vents).
+vector<Point2d> getAllVentLocations(Vent& vent)
+{
+  int min = 0;
+  int max = 0;
+  vector<Point2d> all_locations{};
+  if (isHorizontal(vent)) // (vent.is_horiz)
+  {
+    // No guarantee vents beginning is before end. 
+    min = std::min(vent.loc_begin.x, vent.loc_end.x);
+    max = std::max(vent.loc_begin.x, vent.loc_end.x);
+    // add a point for each missing value of x
+    for (int i = min; i <= max; i += 1)
+    {
+      all_locations.push_back({ i, vent.loc_begin.y });
+    }
+
+  }
+  else if (isVertical(vent)) //(vent.is_vert)
+  {
+    min = std::min(vent.loc_begin.y, vent.loc_end.y);
+    max = std::max(vent.loc_begin.y, vent.loc_end.y);
+    // add a point for each missing value of x
+    for (int i = min; i <= max; i += 1)
+    {
+      all_locations.push_back({ vent.loc_begin.x, i });
+    }
+  }
+  else
+  {
+    // vent is neither vertical or horizontal. According to the
+    // rules, it must be a 45 degree line. Gotta find min and max x and y
+    // values then and create our points between by incrementing.
+    // x can go up while y goes down or up, so it'll take a little
+    // thinkin'.
+
+    // Get starting and ending x values. The difference will be the 
+    // same as that of the y, because diagonal. Need a library that can
+    // create a range of values from a to be regardless of which is larger,
+    // counting up or counting down.
+    Point2d start, end;
+    if (vent.loc_begin.x < vent.loc_end.x)
+    {
+      start = vent.loc_begin;
+      end = vent.loc_end;
+    }
+    else
+    {
+      start = vent.loc_end;
+      end = vent.loc_begin;
+    }
+    int difference = end.x - start.x;
+    auto points = std::vector<Point2d>(difference+1); 
+    // map out all the 45 degree line points between start and end.
+    for (int i = 0; i <= difference; i++)
+    {
+      points[i].x = start.x + i;
+      // y may go from high to low, low to high, so account for them.
+      if (start.y < end.y)
+      {
+        points[i].y = start.y + i;
+      }
+      else
+      {
+        points[i].y = start.y - i;
+      }
+      all_locations.push_back(points[i]);
+    }
+  }
+  return all_locations;
+}
+
+vector<Cell> mapVentsToOceanFloor(vector<Vent>& vents)
+{
+    // TODO Fix this so we're checking every location you would
+    // have between loc_begin and loc_end. For these horizontal
+    // and vertical ones we can probably use something like std::iota
+    // to create the iteration?
+  print("Mapping {0} vents to ocean floor..\n\n", vents.size());
+  vector<Cell> floor{};
+  //int multi_vent_cell_count = 0;
+  for (auto& vent : vents)
+  {
+    vector<Point2d> vent_locations = getAllVentLocations(vent);
+
+    for (auto& location : vent_locations)
+    {
+      vector<Cell>::iterator it;
+      Cell temp_cell = Cell(location);
+      it = std::find(floor.begin(), floor.end(), temp_cell);// floor.find(temp_cell);
+      if (floor.size() == 0 || it == floor.end())
+      {
+        floor.push_back(std::move(temp_cell));
+      }
+      else  // A cell on the ocean floor was already created for this location.
+      {
+        //print(text_styles[Styles::RED_ALERT], "Overlap!");
+        //print(" Floor already contains cell at {0},{1}.\n",
+        //  it->location.x, it->location.y);
+        it->vent_count += 1;
+        //multi_vent_cell_count += 1;
+      }
+    }
+  }
+  //print("\n{0} cells on the floor are split by more than one vent.\n\n", multi_vent_cell_count);
+
+  return floor;
+}
+
+void mapVents2(vector<Vent>& vents, vector<vector<int>>& floor)
+{
+  print("Mapping {0} vents to ocean floor..\n\n", vents.size());
+  for (auto& vent : vents)
+  {
+    vector<Point2d> vent_locations = getAllVentLocations(vent);
+    for (auto& location : vent_locations)
+    {
+      floor[location.x][location.y] += 1;
+    }
+  }
+}
+// Read vent start-end points in from file
+// Collect any vents that are straight horizontal or vertical.
+// Decide dimensions of grid based on 
+// Go through cells and test which of these collected vents 
+// cross the 
+// Mark them in a grid of points that have 
+void checkForOverlappingVents()
+{
+  vector<Vent> all_vents = getVentsFromFile(VENTS_FILE);
+  print("\n\n");
+  print(text_styles[Styles::LOUD_FRIENDLY], "Found {0} vents.", all_vents.size());
+  print("\n\n");
+  auto hv_vents = getHorizontalAndVerticalVents(all_vents);
+  print(text_styles[Styles::STONE], "Found {0} horizontal or vertical vents", hv_vents.size());
+  
+  RectExtents gridExtents = getVentGridExtents(hv_vents);
+  print("\n\n"); 
+  print("Extents: {0},{1} -> {2},{3}\n\n", gridExtents.top_left.x, gridExtents.top_left.y,
+    gridExtents.bot_right.x, gridExtents.bot_right.y);
+
+  //ocean floor mapped to a grid
+  auto floor_grid = vector<vector<int>> (1024, vector<int>(1024,0));
+  
+  // Part 1 was horizontal, vertical vents only.
+  //mapVents2(hv_vents, floor_grid);
+  // Part 2 is all vents.
+  mapVents2(all_vents, floor_grid);
+
+  int overlaps = 0;
+  for (int i = 0; i < 1024; i++)
+  {
+    for (int j = 0; j < 1024; j++)
+    {
+      if (floor_grid[i][j] > 1) overlaps += 1;
+    }
+  }
+  print("Overlaps: {0}\n", overlaps);
+  
+}
+
+int main() {
+  //clearScreen();
+  createStyles();
+  //greetInColour();
+  
+  checkForOverlappingVents();
+}
--- day6-lanternfish/main.cpp
+++ day6-lanternfish/main.cpp
@@ -14,8 +14,12 @@
 #include <fstream>
 #include <set>
 #include <cmath>
+#include <thread>
+#include <future>
+#include <atomic>
 #include "include/ise102.h"
 #include "include/Point2d.h"
+#include "include/timer.h"
 using namespace std;
 using scn::scan;
 using textpixels::Point2d;
@@ -28,6 +32,8 @@ enum Styles
   RED_ALERT
 };
 
+const uint8_t ADULT_SPAWN_CYCLE = 6;
+const uint8_t JUVENILE_SPAWN_CYCLE = 8;
 const string FISH_FILE = "lanternfish.txt";
 std::map<int, fmt::text_style> text_styles {};
 
@@ -74,12 +80,13 @@ void greetInColour()
   delay(1000);
 }
 
-vector<int> getFishFromFile(string file_path)
+vector<uint8_t> getFishFromFile(string file_path)
 {
   // Grab the file handle in a way scn can keep it alive.
   scn::owning_file fish_file(file_path.c_str(), "r");
-  vector<int> fish;
-  //vents.reserve(1000);
+  vector<uint8_t> fish;
+  long long int estimated_fish = 10'000'000'000;
+  fish.reserve(estimated_fish);
   std::string line{};
   auto result = scn::make_result(fish_file);
 
@@ -91,7 +98,7 @@ vector<int> getFishFromFile(string file_path)
     //size_t pos = 0;
     for(const char& c : line)
     {
-      if (c != ',') fish.push_back(c-'0');
+      if (c != ',') fish.push_back(static_cast<uint8_t>(c-'0'));
     }
     // grab an int, a comma, an int, a comma over and over.
     // maybe getch is better than getline.
@@ -102,10 +109,10 @@ vector<int> getFishFromFile(string file_path)
   return fish;
 }
 
-void printFishes(vector<int>& fishes, string message)
+void printFishes(vector<uint8_t>& fishes, string message)
 {
   print(message);
-  for( int fishy : fishes )
+  for( uint8_t fishy : fishes )
   {
     print("{0},", fishy);
   }
@@ -113,28 +120,14 @@ void printFishes(vector<int>& fishes, string message)
   print("\b\n");
 }
 
-void simulateFishes(vector<int>& fishes, int days_to_sim)
+atomic_uint64_t fishes_spawned_today = 0;
+
+
+void simulateDay(vector<uint8_t> fishes, const uint64_t lower, const uint64_t upper)
 {
-  const int ADULT_SPAWN_CYCLE = 6;
-  const int JUVENILE_SPAWN_CYCLE = 8;
-  int days_left = days_to_sim;
-  // each day
-  // subtract 1 from all fish unless they're on 0,
-  // in chich case loop to 6.
-  // if you had to loop, spawn another fish into the collection with
-  // 8 days remaining.
-  print("Simulating...");
-  for(; days_to_sim > 0; days_to_sim--)
-  {
-    // will this feed us fish for ever when we use for - in?
-    // No, iterator isn't updated after loop begins.
-    // for ( auto& fishy : fishes)
-    // using auto, the addition of a new fish led to skipping of one entry,
-    // so switching to a manual iterator. Also grabbing the fish count
-    // before growing the vector in case of hijinx.
-    print("Day {0}.\n", days_left - days_to_sim + 1);
-    int fish_count = fishes.size();
-    for (int j = 0; j < fish_count; j++)
+  int fishes_spawned = 0;
+  // work with the reference, remember to use ref to call this so the reference actually works.
+  for (uint64_t j = 0; j <= upper; j++)
     {
       //int& fishy = fishes[j];
       if ( fishes[j] > 0 )
@@ -143,32 +136,236 @@ void simulateFishes(vector<int>& fishes, int days_to_sim)
       } else
       {
         fishes[j] = ADULT_SPAWN_CYCLE;
-        fishes.push_back(JUVENILE_SPAWN_CYCLE);
+        //fishes.push_back(JUVENILE_SPAWN_CYCLE);
+        // update atomic new fish counter
+        fishes_spawned += 1;
       }
     }
-    // printFishes(fishes, "After " + to_string(days_left - days_to_sim +1) + " days: ");
-  }
+    fishes_spawned_today += fishes_spawned;
 }
+
+int func() { return 1; }
+
+uint64_t simulateFishPortion(vector<uint8_t> fishes, int days_to_sim)
+{
+  // simulaaate
+  //delay(50);
+  int days_left = days_to_sim;
+  fishes.reserve(1'000'000'000); // reserve room for 1 billion fish
+  for(; days_left > 0; days_left--)
+  {
+    uint64_t fishes_count = fishes.size();
+    uint64_t fishes_spawned_today = 0;
+    // work with the reference, remember to use ref to call this so the reference actually works.
+    for (uint64_t i = 0; i < fishes_count; i++)
+    {
+      if ( fishes[i] > 0 )
+      {
+        fishes[i] -= 1;
+      } else
+      {
+        fishes[i] = ADULT_SPAWN_CYCLE;
+        //fishes.push_back(JUVENILE_SPAWN_CYCLE);
+        // update atomic new fish counter
+        fishes_spawned_today += 1;
+      }
+    }
+    if (fishes_spawned_today > 0) 
+      // Add newly spawned fish to the collection as juveniles (longer spawn cycle)
+      fishes.resize(fishes_count + fishes_spawned_today, JUVENILE_SPAWN_CYCLE);
+  }
+    
+    // do all the fish spawn day iterating
+  // if any have spawned, add the spawned dudes to this vector.
+  // finally, return a vector (i guess each will need one named)
+  // Actually, only need to return the size of the vector at end of x days.
+  // this would be a good time to use a future: 
+  // https://stackoverflow.com/questions/7686939/c-simple-return-value-from-stdthread
+  return static_cast<uint64_t>(fishes.size()); // final number of fishies goes here
+}
+
+uint64_t simulateFishes(vector<uint8_t>& starting_fishes, const int days_to_sim)
+{
+  dmac::Timer timer;
+  
+  const uint64_t starting_fish_count = starting_fishes.size();
+  const int num_threads=8;
+  timer.start();
+  vector<future<uint64_t>> futures (num_threads);
+  for (int f = 0; f < num_threads; f++)
+  {
+    //std::future<uint64_t> ret = std::async(&func);
+    int start = (starting_fish_count / num_threads) * f;
+    int end = (starting_fish_count / num_threads) * (f+1);
+    if (f == num_threads-1) end = starting_fish_count;
+    print("creating portion with indices {0} to {1}\n", start, end);
+    vector<uint8_t> portion (starting_fishes.begin()+start,
+                            starting_fishes.begin()+end);
+    futures[f] = std::async(
+        simulateFishPortion, portion, days_to_sim);
+  }
+  vector<uint64_t> sim_results(num_threads,0);
+  uint64_t total_fish_after_sim {0};
+  for( int f = 0; f < num_threads; f++ )
+  {
+    sim_results[f] = futures[f].get();
+    total_fish_after_sim += sim_results[f];
+  }
+  print(text_styles[Styles::LOUD_FRIENDLY], "\n\nTotal fish after {0} days: {1}.\n\n",
+      days_to_sim, total_fish_after_sim);
+  //int i = ret.get();
+  //int days_left = days_to_sim;
+  // each day
+  // subtract 1 from all fish unless they're on 0,
+  // in chich case loop to 6.
+  // if you had to loop, spawn another fish into the collection with
+  // 8 days remaining.
+  /*print("Simulating...");
+  int num_threads = 4;
+  // create num_threads collections containing their portion of the starting_fishes.
+  // Pass those containers to the threads, so noone's sharing.
+  const uint64_t starting_fish_count = starting_fishes.size();
+  vector<thread> threads {};
+  for(int i =0; i < num_threads; i++)
+  {
+    // create a container with 1/num_threads of the starting fishes
+    // pass it to a thread to simulate. 
+    int start = (starting_fish_count / 4) * i;
+    int end = (starting_fish_count / 4) * i+1;
+    if (i == num_threads-1) end = starting_fish_count;
+    vector<uint8_t> portion (starting_fishes.begin()+start,
+                            starting_fishes.begin()+end);
+    threads.push_back(thread(simulateFishPortion, portion, days_to_sim));
+  }
+
+    for ( auto& th : threads )
+    {
+      if(th.joinable())
+        th.join();
+    }
+    */
+  /*
+  for(; days_left > 0; days_left--)
+  {
+    vector<thread> threads {};
+    // Using manual iterator because ranged loop was missing entries when I added
+    // to vector. Also grabbing the fish count before growing the vector in case of hijinx.
+    
+    
+    const uint64_t fish_count = fishes.size();
+    fishes_spawned_today = 0;
+    // Divide collection into 4 parts so it can be sent to four threads.
+    // Will a reference work? will this be too much memory usage/copying?
+    // Create some threads
+    // Ask each to process fish_count / num_threads fish, have them record how many
+    // fish must be added to the vector. When they are done, they can lock the 
+    // "fish_spawned_today" variable and add to it.
+    for(int i = 0; i<num_threads; i++)
+    {
+      uint64_t start = (fish_count / num_threads) * i;
+      uint64_t end = (fish_count / num_threads) * (i+1);
+      if (i == num_threads-1) end = fish_count - 1;
+      threads.push_back(thread(simulateDay, start, end));
+    }
+    for ( auto& th : threads )
+    {
+      if(th.joinable())
+        th.join();
+    }*/
+    timer.stop();
+    //When all threads have joined,
+    // use fishes.resize(fish_spawned_today, JUVENILE_SPAWN_CYCLE) to add all the
+    // extras at once.. or use a for loop if that reallocates too much.
+    /*if (fishes_spawned_today > 0) 
+      fishes.resize(fish_count + fishes_spawned_today, JUVENILE_SPAWN_CYCLE);
+    // printFishes(fishes, "After " + to_string(days_left - days_to_sim +1) + " days: ");
+    print("Day {0}, {1} fish.\n", days_to_sim - days_left + 1, fishes.size()); 
+  }*/
+  print("Time to sim: {0} seconds.", timer.timeTakenSeconds());
+  return(total_fish_after_sim);
+}
+
+
+/*void simulateFishesAllOneContainer(const int days_to_sim)
+{
+  
+  int days_left = days_to_sim;
+  dmac::Timer timer;
+  // each day
+  // subtract 1 from all fish unless they're on 0,
+  // in chich case loop to 6.
+  // if you had to loop, spawn another fish into the collection with
+  // 8 days remaining.
+  print("Simulating...");
+  timer.start();
+  int num_threads = 4;
+  for(; days_left > 0; days_left--)
+  {
+    vector<thread> threads {};
+    // Using manual iterator because ranged loop was missing entries when I added
+    // to vector. Also grabbing the fish count before growing the vector in case of hijinx.
+    
+    
+    const uint64_t fish_count = fishes.size();
+    fishes_spawned_today = 0;
+    // Divide collection into 4 parts so it can be sent to four threads.
+    // Will a reference work? will this be too much memory usage/copying?
+    // Create some threads
+    // Ask each to process fish_count / num_threads fish, have them record how many
+    // fish must be added to the vector. When they are done, they can lock the 
+    // "fish_spawned_today" variable and add to it.
+    for(int i = 0; i<num_threads; i++)
+    {
+      uint64_t start = (fish_count / num_threads) * i;
+      uint64_t end = (fish_count / num_threads) * (i+1);
+      if (i == num_threads-1) end = fish_count - 1;
+      threads.push_back(thread(simulateDay, start, end));
+    }
+    for ( auto& th : threads )
+    {
+      if(th.joinable())
+        th.join();
+    }
+    timer.stop();
+    //When all threads have joined,
+    // use fishes.resize(fish_spawned_today, JUVENILE_SPAWN_CYCLE) to add all the
+    // extras at once.. or use a for loop if that reallocates too much.
+    if (fishes_spawned_today > 0) 
+      fishes.resize(fish_count + fishes_spawned_today, JUVENILE_SPAWN_CYCLE);
+    // printFishes(fishes, "After " + to_string(days_left - days_to_sim +1) + " days: ");
+    print("Day {0}, {1} fish.\n", days_to_sim - days_left + 1, fishes.size());
+  }
+  print("Time to sim: {0} seconds.", timer.timeTakenSeconds());
+}
+*/
 
 int main() {
   //clearScreen();
   createStyles();
+  unsigned int num_threads_supported = std::thread::hardware_concurrency();
+  //std::cout << n << " concurrent threads are supported.\n";
   //greetInColour();
-  const int DAYS_TO_SIM = 160; //80; //18;
+  const int DAYS_TO_SIM = 200; //80; //18;
   
-  //vector<int> fishes {3,4,3,1,2};
-  auto fishes = getFishFromFile(FISH_FILE);
-  print("Got {0} lantern fish.\n", fishes.size());  
+  //vector<uint8_t> fishes {3,4,3,1,2};
+  // TODO: turn fish into much smaller ints (8 bit) to get optimsation.
+  //vector<uint8_t> fishes;
+  vector<uint8_t> starting_fishes = getFishFromFile(FISH_FILE);
+  print("Got {0} lantern fish.\n", starting_fishes.size());  
 
-   
   //printFishes(fishes, "Initial state: ");
-  // TODO: Do a test using the example data first to make sure it works.
-  // TODO: Simulate the spawning of lanternfish, starting with these 80, for 300 days. 
   // FEARRR!: Will it become too much to store? Is this an exercise in allocation? Will it crash?
-  simulateFishes(fishes, DAYS_TO_SIM);
+  
+  // TODO: Solve the problem of the great growth of the vector leading to
+  // slow-down and memory usage - already serious at 160 days - that will
+  // make it hard to hit 256.   
+  uint64_t fishes_at_end = simulateFishes(starting_fishes, DAYS_TO_SIM);
+  std::string answer {};
+  scn::prompt("Are we good?", "{0}", answer);
 
   // Might need threads to make this perform beyond day 160,
-  // though you'd need locks during adds.
-  print("\nAfter {0} days we have ", DAYS_TO_SIM);
-  print(text_styles[Styles::LOUD_FRIENDLY], "{0} fish.\n", fishes.size());
+  // though you'd need locks during adds.j
+  // No more threads than cpu cores either.
+  //print("\nAfter {0} days we have ", DAYS_TO_SIM);
+  //print(text_styles[Styles::LOUD_FRIENDLY], "{0} fish.\n", fishes.size());
 }
